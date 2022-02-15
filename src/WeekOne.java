4b318a6b5c1ac81f762206ccdafd18eaa5670ee9
Kyle Schrader <86627481+schrader88@users.noreply.github.com>
2022-02-15 16:33:04 -0600

    starting week one hacker rank

---
+++ src/WeekOne.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+
+public class WeekOne {
+//    public static void plusMinus(int[] array) {
+//        int pos = 0;
+//        int neg = 0;
+//        int zero = 0;
+//
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] > 0) {
+//                pos++;
+//            } else if (array[i] < 0) {
+//                neg++;
+//            } else {
+//                zero++;
+//            }
+//        }
+//
+//
+//    }
+}
